10cc9d113ff343073f7eb3dff24e97450cca0892
linlany <465057951@qq.com>
2020-10-22 16:44:46 +0800

    flask的内容1.0

---
+++ flaskproject/index.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+import json
+import os
+with open(os.path.join(os.path.join(os.path.expanduser("~"), "%s" % "anadata".lower()),"data.json"),'r') as load_f:
+    data_dict = json.load(load_f)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    my_dict = data_dict
+
+    # render_template方法:渲染模板
+    # 参数1: 模板名称  参数n: 传到模板里的数据
+    return render_template('hello.html',
+                           my_dict=my_dict)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=False)
