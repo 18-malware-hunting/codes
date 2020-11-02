20aab586a95cfb47e4d04e2d4a4b934597281743
summery <55629269+Song-233@users.noreply.github.com>
2020-11-03 00:43:41 +0800

    Update preproc_1.py

--- data_proc/preproc_1.py
+++ data_proc/preproc_1.py
@@ -183,7 +183,7 @@ for filename in sorted(filenames):
 
     #store as file
     
-    fea = open('feature.csv','wb')
+    fea = open('feature.csv','a+')
     csv_writer=csv.writer(fea)
     #csv_writer.writerow(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
         
